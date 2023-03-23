8dd77a6a3ca540a1e6f5560b2f0755d5055372e4
github-Dheeraj <65020178+github-Dheeraj@users.noreply.github.com>
2023-03-23 14:35:35 +0530

    property controllers change

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -19,53 +19,64 @@ exports.createProperty = async (req, res) => {
             _balcony,    //Int?
             _furnishing, //Furnish?
         } = req.body
-        console.log("user Id", req.query.id)
-        let sellerData = await prisma.Seller.findUnique(({
+
+        let userData = await prisma.User.findUnique({
             where: {
-                userId: parseInt(req.query.id)
+                email: req.body._email
             }
-        }))
-        console.log("SellerData", sellerData)
+        })
 
-        let ObjUrls = []
-        if (sellerData) {
-            if (req.files && req.files.length > 0) {
-                for (var i = 0; i < req.files.length; i++) {
-                    // console.log(req.files[i]);
-                    let objUrl = await uploadToS3(req.files[i].buffer, req.files[i].originalname);
-                    console.log(" Aws url", objUrl.Location);
-                    ObjUrls.push(objUrl.Location);
+        if (userData) {
+            let sellerData = await prisma.Seller.findUnique(({
+                where: {
+                    userId: userData.id
                 }
-            }
-            console.log("seller Id", sellerData.id)
-            let property = await prisma.Property.create({
-                data: {
-                    sellerId: parseInt(sellerData.id),
-                    title: _title,
-                    address: _address,
-                    cost: parseInt(_cost),
-                    deposit: parseInt(_deposit),
-                    bedrooms: parseInt(_bedrooms),
-                    washRooms: parseInt(_washRooms),
-                    floorNum: parseInt(_floorNum),
-                    floorArea: parseInt(_floorArea),
-                    balcony: parseInt(_balcony),
-                    parking: _parking,
-                    furnishing: _furnishing,
-                    mediaUrls: ObjUrls
-                }
-            })
-            if (property) {
-                console.log("Property created");
-                return new HTTPResponse(res, true, 200, null, null, { property });
+            }))
+            console.log("SellerData", sellerData)
 
+            let ObjUrls = []
+            if (sellerData) {
+                if (req.files && req.files.length > 0) {
+                    for (var i = 0; i < req.files.length; i++) {
+                        // console.log(req.files[i]);
+                        let objUrl = await uploadToS3(req.files[i].buffer, req.files[i].originalname);
+                        console.log(" Aws url", objUrl.Location);
+                        ObjUrls.push(objUrl.Location);
+                    }
+                }
+                console.log("seller Id", sellerData.id)
+                let property = await prisma.Property.create({
+                    data: {
+                        sellerId: parseInt(sellerData.id),
+                        title: _title,
+                        address: _address,
+                        cost: parseInt(_cost),
+                        deposit: parseInt(_deposit),
+                        bedrooms: parseInt(_bedrooms),
+                        washRooms: parseInt(_washRooms),
+                        floorNum: parseInt(_floorNum),
+                        floorArea: parseInt(_floorArea),
+                        balcony: parseInt(_balcony),
+                        parking: _parking,
+                        furnishing: _furnishing,
+                        mediaUrls: ObjUrls
+                    }
+                })
+                if (property) {
+                    console.log("Property created");
+                    return new HTTPResponse(res, true, 200, null, null, { property });
+
+                } else {
+
+                    return new HTTPError(res, 400, null, "internal server error")
+                }
             } else {
-
-                return new HTTPError(res, 400, null, "internal server error")
+                return new HTTPError(res, 404, null, "Seller not initialized")
             }
         } else {
-            return new HTTPError(res, 404, null, "Seller not initialized")
+            return new HTTPError(res, 404, null, "User not found")
         }
+
     } catch (err) {
         console.log(err);
 
