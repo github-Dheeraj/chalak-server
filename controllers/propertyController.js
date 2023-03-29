3fcfeea8e080bde39ad42fc2e8452e8870b94cff
github-Dheeraj <65020178+github-Dheeraj@users.noreply.github.com>
2023-03-29 12:32:23 +0530

    testing the property issue

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -100,7 +100,7 @@ exports.updateProperty = async (req, res, next) => {
             _propertyId,
             _title,      //String
             _address,    //String?
-            _cost,       //Int?
+            _rent,       //Int?
             _deposit,    //Int?
             _bedrooms,   //Int?
             _washRooms,  //Int?
@@ -112,17 +112,19 @@ exports.updateProperty = async (req, res, next) => {
             _furnishings, //String[]
             _isActive
         } = req.body
-        console.log("files", req.files)
-        if (_parking.toLowerCase() === "true") { _parking = true } else {
-            _parking = false
-        }
-        if (_isActive.toLowerCase() === "true") { _isActive = true } else {
-            _parking = false
+        console.log("files", req.body)
+        if (_parking) {
+            if (_parking.toLowerCase() === "true") { _parking = true } else {
+                _parking = false
+            }
+            if (_isActive.toLowerCase() === "true") { _isActive = true } else {
+                _parking = false
+            }
         }
 
-        let sellerData = await prisma.Seller.findUniqueOrThrow(({
+        let sellerData = await prisma.Property.findUnique(({
             where: {
-                userId: parseInt(req.query.id)
+                id: parseInt(_propertyId)
             }
         }))
         console.log("sellerData", sellerData)
@@ -145,7 +147,7 @@ exports.updateProperty = async (req, res, next) => {
                 data: {
                     title: _title,
                     address: _address,
-                    cost: parseInt(_cost),
+                    rent: parseInt(_rent),
                     deposit: parseInt(_deposit),
                     bedrooms: parseInt(_bedrooms),
                     washRooms: parseInt(_washRooms),
@@ -157,7 +159,8 @@ exports.updateProperty = async (req, res, next) => {
                     furnishings: _furnishings,
                     mediaUrls: {
                         push: ObjUrls
-                    }
+                    },
+                    isActive: _isActive
                 }
             })
 
@@ -179,6 +182,52 @@ exports.updateProperty = async (req, res, next) => {
 
 }
 
+exports.updatePropertyStatus = async (req, res) => {
+    try {
+
+        let {
+            _propertyId,
+            _isActive
+        } = req.body
+        console.log("files", req.body)
+
+
+        let sellerData = await prisma.Property.findUnique(({
+            where: {
+                id: parseInt(_propertyId)
+            }
+        }))
+        console.log("sellerData", sellerData)
+        if (sellerData) {
+
+            console.log("seller data", sellerData)
+            let property = await prisma.Property.update({
+                where: {
+                    id: parseInt(_propertyId),
+                },
+                data: {
+                    isActive: _isActive
+
+                }
+            })
+
+            if (property) {
+                console.log("Property updated");
+                return new HTTPResponse(res, true, 200, null, null, { property });
+
+            } else {
+                return new HTTPError(res, 400, null, "internal server error")
+            }
+        } else {
+            return new HTTPError(res, 400, null, "Seller not found")
+        }
+    } catch (err) {
+        console.log(err);
+
+        return new HTTPError(res, 400, err, "internal server error")
+    }
+
+}
 exports.getPropertyDetails = async (req, res, next) => {
     try {
         let property = await prisma.Property.findUnique({
@@ -216,6 +265,42 @@ exports.deleteProperty = async (req, res, next) => {
         return new HTTPError(res, 400, err, "internal server error")
     }
 
+}
+
+exports.getPropertyListUser = async (req, res, next) => {
+    try {
+        let userExist = await prisma.User.findUnique({
+            where: {
+                email: req.body._email
+            },
+        })
+        if (userExist) {
+            console.log("USer found");
+
+            let sellerExist = await prisma.Seller.findUnique({
+                where: {
+                    userId: userExist.id
+                },
+            })
+            console.log("Seller found", sellerExist)
+            if (sellerExist) {
+                let allProperty = await prisma.Property.findMany({
+                    where: {
+                        sellerId: sellerExist.id
+                    }
+                })
+                return new HTTPResponse(res, true, 200, null, null, allProperty);
+
+            }
+
+        } else {
+            return new HTTPError(res, 400, null, "Property not found")
+        }
+    } catch (err) {
+        console.log(err);
+
+        return new HTTPError(res, 400, err, "internal server error")
+    }
 }
 
 
--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -3,15 +3,20 @@ const router = express.Router()
 const HTTPError = require("../utils/HTTPError");
 const { HTTPResponse } = require("../utils/httpResponse");
 const {
-    createProperty, updateProperty, deleteProperty, getPropertyDetails
+    createProperty, updateProperty, deleteProperty, getPropertyDetails, getPropertyListUser, updatePropertyStatus
 } = require("../controllers/propertyController")
 const { upload } = require("../utils/awsStorage")
 
 
 router.route("/create").post(upload.array("images", 15), createProperty)
 router.route("/update").post(upload.array("images", 15), updateProperty)
+router.route("/updateStatus").post(updatePropertyStatus)
+
 router.route("/getDetail").get(getPropertyDetails)
+
 router.route("/delete").post(deleteProperty)
+
+router.route("/getProperties").get(getPropertyListUser)
 router.get('/', function (req, res) {
     res.send('Welcome to our API!');
 });
