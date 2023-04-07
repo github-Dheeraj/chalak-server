375219476f03bebf1294fe8f183a46be310ce334
github-Dheeraj <65020178+github-Dheeraj@users.noreply.github.com>
2023-04-07 23:07:53 +0530

    admin controllers

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const { signUp } = require('../utils/whatsApp')
-
+const { getInTouchAdmin } = require('../controllers/adminController')
 router.route('/waSignUp').post(signUp)
-
+router.route('/getInTouch').post(getInTouchAdmin)
 module.exports = router
